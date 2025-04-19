5683cd896d3d8c85e7cf7552a06232a4606fafb1
armandhuka <shukaarman7585@gmail.com>
2025-04-19 10:17:28 +0530

    Added Vercel Analytics

--- src/main.jsx
+++ src/main.jsx
@@ -2,12 +2,14 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
+import { Analytics } from '@vercel/analytics/react'
 import { ThemeProvider } from './Context/ThemeContext'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <ThemeProvider>
       <App />
+      <Analytics />
     </ThemeProvider>
   </StrictMode>,
 )
